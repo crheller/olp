7bf2f6ac94179ff465005bbcbc7dfb937880cd8e
crheller <charlieheller95@gmail.com>
2021-01-21 13:59:56 -0800

    per trial on decoding axis

--- helpers.py
+++ helpers.py
@@ -47,7 +47,7 @@ def get_fg_bg_full_overlap(rec):
     return bg_fg
 
 
-def get_fg_pcs(rec):
+def get_fg_pcs(rec, collapse=False):
     """
     Get top pc axes of FG evoked responses
     """
@@ -57,18 +57,28 @@ def get_fg_pcs(rec):
 
     fgs, bgs = get_fg_bg_epochs(rec)
     fgs1 = [f for f in fgs if f.endswith('0-1')]
-    fg_resp = np.zeros((len(fgs1), nCells))
+    if collapse:
+        fg_resp = np.zeros((len(fgs1), nCells))
+    else: 
+        fg_resp = []
     for i, fg in enumerate(fgs1):
-        resp = rec['resp'].extract_epoch('STIM_null_'+fg)[:, :, prestim:-poststim].sum(axis=-1).mean(axis=0)
-        prespont = rec['resp'].extract_epoch('STIM_null_'+fg)[:, :, 0:prestim].sum(axis=-1).mean(axis=0)
-        fg_resp[i, :] = resp - prespont
+        if collapse:
+            resp = rec['resp'].extract_epoch('STIM_null_'+fg)[:, :, prestim:-poststim].sum(axis=-1).mean(axis=0)
+            prespont = rec['resp'].extract_epoch('STIM_null_'+fg)[:, :, 0:prestim].sum(axis=-1).mean(axis=0)
+            fg_resp[i, :] = resp - prespont
+        else:
+            resp = rec['resp'].extract_epoch('STIM_null_'+fg)[:, :, prestim:-poststim].mean(axis=0)
+            prespont = rec['resp'].extract_epoch('STIM_null_'+fg)[:, :, 0:prestim].mean(axis=(0, -1))
+            fg_resp.append((resp.T - prespont))
 
+    if type(fg_resp) == list:
+        fg_resp = np.concatenate(fg_resp, axis=0)
     fg_pca = PCA()
     fg_pca.fit(fg_resp)
     return fg_pca.components_
 
 
-def get_bg_pcs(rec):
+def get_bg_pcs(rec, collapse=False):
     """
     Get top pc axes of BG evoked responses
     """
@@ -78,12 +88,55 @@ def get_bg_pcs(rec):
     
     fgs, bgs = get_fg_bg_epochs(rec)
     bgs1 = [b for b in bgs if b.endswith('0-1')]
-    bg_resp = np.zeros((len(bgs1), nCells))
+    if collapse:
+        bg_resp = np.zeros((len(bgs1), nCells))
+    else:
+        bg_resp = []
     for i, bg in enumerate(bgs1):
-        resp = rec['resp'].extract_epoch('STIM_' + bg + '_null')[:, :, prestim:-poststim].sum(axis=-1).mean(axis=0)
-        prespont = rec['resp'].extract_epoch('STIM_' + bg + '_null')[:, :, 0:prestim].sum(axis=-1).mean(axis=0)
-        bg_resp[i, :] = resp - prespont
+        if collapse:
+            resp = rec['resp'].extract_epoch('STIM_' + bg + '_null')[:, :, prestim:-poststim].sum(axis=-1).mean(axis=0)
+            prespont = rec['resp'].extract_epoch('STIM_' + bg + '_null')[:, :, 0:prestim].sum(axis=-1).mean(axis=0)
+            bg_resp[i, :] = resp - prespont
+        else:
+            resp = rec['resp'].extract_epoch('STIM_' + bg + '_null')[:, :, prestim:-poststim].mean(axis=0)
+            prespont = rec['resp'].extract_epoch('STIM_' + bg + '_null')[:, :, 0:prestim].mean(axis=(0, -1))
+            bg_resp.append((resp.T - prespont))
 
+    if type(bg_resp) == list:
+        bg_resp = np.concatenate(bg_resp, axis=0)
     bg_pca = PCA()
     bg_pca.fit(bg_resp)
     return bg_pca.components_
+
+
+def get_decoder(rec, fg_epoch, bg_epoch, collapse=False):
+    """
+    Get decoding axis for each time bin for the given epoch pair
+    """
+
+    prestim = rec['resp'].extract_epoch('PreStimSilence').shape[-1]
+    poststim = rec['resp'].extract_epoch('PostStimSilence').shape[-1]
+    tbins = rec['resp'].extract_epoch('REFERENCE').shape[-1]
+    nCells = rec['resp'].shape[0]
+
+    fg_resp = rec['resp'].extract_epoch('STIM_null_'+fg_epoch) 
+    bg_resp = rec['resp'].extract_epoch('STIM_'+bg_epoch+'_null') 
+
+    decoding_axes = np.zeros((tbins, nCells))
+    if collapse:
+        # single decoding axis, repped out for all tbins
+        fr = fg_resp[:, :, prestim:-poststim].transpose([0, 2, 1]).reshape(-1, nCells)
+        br = bg_resp[:, :, prestim:-poststim].transpose([0, 2, 1]).reshape(-1, nCells)
+        axis = fr.mean(axis=0) - br.mean(axis=0)
+        axis /= np.linalg.norm(axis)
+        decoding_axes = np.tile(axis, [tbins, 1])
+    else:
+        # different decoding axis for each time point
+        for t in range(fg_resp.shape[-1]):
+            fr = fg_resp[:, :, t]
+            br = bg_resp[:, :, t]
+            axis = fr.mean(axis=0) - br.mean(axis=0)
+            axis /= np.linalg.norm(axis)
+            decoding_axes[t, :] = axis
+
+    return decoding_axes
--- per_trial_decoding.py
+++ per_trial_decoding.py
@@ -1,8 +1,8 @@
 """
 Over the course of the trial, plot the projections onto:
     - decoding axis (computed per bin? If so, show similarity over time with matrix...)
-    - FG encoding axis (PC1 of fg in isolation)
-    - BG encoding axis (PC1 of bg in isolation)
+    - FG encoding axis (PC1 of fg in isolation)?
+    - BG encoding axis (PC1 of bg in isolation)?
 """
 from nems_lbhb.baphy_experiment import BAPHYExperiment
 import numpy as np
@@ -12,7 +12,7 @@ from sklearn.decomposition import PCA
 
 mfile = '/auto/data/daq/Armillaria/ARM021/ARM021b14_p_OLP.m'
 
-options = {'rasterfs': 10, 'resp': True}
+options = {'rasterfs': 20, 'resp': True}
 manager = BAPHYExperiment(parmfile=[mfile])
 rec = manager.get_recording(**options)
 rec['resp'] = rec['resp'].rasterize()
@@ -31,10 +31,9 @@ bg_fg_epochs = helpers.get_bg_fg_transitions(rec)
 # get projection axes
 fg_pcs = helpers.get_fg_pcs(rec)
 bg_pcs = helpers.get_bg_pcs(rec)
-decoder = helpers.get_decoder(rec)
 
 # project / plot single trial data for fg to bg + fg transitions
-proj_axis = bg_pcs[0, :]
+proj_axis = fg_pcs[0, :]
 f, ax = plt.subplots(len(fg_bg_epochs), 1, figsize=(8, 12), sharey=True, sharex=True)
 
 r_fgbg = rec['resp'].extract_epochs(fg_bg_epochs)
@@ -51,6 +50,49 @@ for trans, a in zip(fg_bg_epochs, ax):
     a.axvline(prestim + 0.5, linestyle='--', color='red')
     a.axvline(prestim, linestyle='--', color='k')
     a.axvline(poststim, linestyle='--', color='k')
+
+f.tight_layout()
+
+
+# if collapse, single axis for each time point
+
+fg_epoch = '01Fight_Squeak-0-1'
+bg_epoch = '03Insect_Buzz-0-1'
+fg_bg_epoch = 'STIM_03Insect_Buzz-0.5-1_01Fight_Squeak-0-1'
+decoder = helpers.get_decoder(rec, fg_epoch, bg_epoch, collapse=False)
+
+f = plt.figure(figsize=(11, 3))
+
+tser = plt.subplot2grid((1, 5), (0, 0), colspan=3)
+sim = plt.subplot2grid((1, 5), (0, 3), colspan=2)
+
+fg_resp = rec['resp'].extract_epoch('STIM_null_'+fg_epoch)
+bg_resp = rec['resp'].extract_epoch('STIM_'+bg_epoch+'_null')
+fg_bg_resp = rec['resp'].extract_epoch(fg_bg_epoch)
+
+# project single trials onto each decoding axis
+# responses in isolation
+fg_resp = np.concatenate([fg_resp[:, :, i] @ decoder[[i], :].T for i in range(decoder.shape[0])], axis=-1)
+bg_resp = np.concatenate([bg_resp[:, :, i] @ decoder[[i], :].T for i in range(decoder.shape[0])], axis=-1)
+# response to combined
+fg_bg_resp = np.concatenate([fg_bg_resp[:, :, i] @ decoder[[i], :].T for i in range(decoder.shape[0])], axis=-1)
+
+tser.plot(time, fg_resp.mean(axis=0), '--', label='fg (isolation)', color='tab:blue', lw=2)
+tser.plot(time, bg_resp.mean(axis=0), '--', label='bg (isolation)', color='tab:orange', lw=2)
+m = fg_bg_resp.mean(axis=0)
+sem = fg_bg_resp.std(axis=0)
+tser.plot(time, m, lw=2, color='grey')
+tser.fill_between(time, m-sem, m+sem, lw=0, alpha=0.5, color='grey', label='Combination')
+tser.axvline(prestim+0.5, color='k', lw=2, label='transition', zorder=0)
+
+tser.legend(frameon=False, bbox_to_anchor=(1,1), loc="upper left")
+tser.set_ylabel('Decoding axis projection')
+tser.set_xlabel('Trial time')
+tser.set_title(fg_bg_epoch)
+tser.set_xlim((time[0], time[-1]))
+
+sim.imshow(np.abs(decoder @ decoder.T), aspect='auto', cmap='Reds', vmin=0, vmax=1, extent=[time[0], time[-1], time[0], time[-1]])
+sim.set_title('Decoding axes similarity')
 
 f.tight_layout()
 
